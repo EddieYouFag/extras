ee44d5ded7bc4c897a78fe0f7dfa19dbf63f6703
th3unkn0n <40244622+th3unkn0n@users.noreply.github.com>
2019-10-03 02:24:31 +0530

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,17 +1,19 @@
 #!/bin/env python3
+re="\033[1;31m"
+cy="\033[1;36m"
 import os, time, signal
 os.system('clear')
 def handler(signum, frame):
-	print('[+] You Cant Esacpe Bitch, LoL')
+	print(re+'[+] You Cant Esacpe Bitch, LoL')
 	time.sleep(3)
 signal.signal(signal.SIGTSTP, handler)
 def loop():
 	while True:
-		print("[+] You Have Been Fucked up")
+		print(cy+"[+] You Have Been Fucked up")
 		try:
 			time.sleep(0.1)
 		except KeyboardInterrupt:
-			print('[+] You Cant Esacpe Bitch, LoL')
+			print(cy+'[+] You Cant Esacpe Bitch, LoL')
 			time.sleep(3)
 			loop()
 loop()
